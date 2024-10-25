3bed7e20b9e0418698278a2f333618be1ccda9c4
gokulnathanT <mailtogokul1727@gmail.com>
2024-10-25 14:37:44 +0530

    first commit

---
+++ src/main/java/com/example/Java_React/Controller/Controller.java
@@ -0,0 +1,12 @@
+package com.example.Java_React.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+}
